389ab898a078144f376697420498eb3875574dd5
Joao <johnynecas@hotmail.com>
2017-11-19 19:16:40 +0000

    created Menu class and constructor

---
+++ MariosRestaurant/src/main/java/models/Menu.java
@@ -0,0 +1,17 @@
+package models;
+
+import interfaces.MenuItem;
+
+import java.util.ArrayList;
+
+public class Menu {
+    private ArrayList<MenuItem> menu;
+
+
+    public Menu(){
+        this.menu= new ArrayList();
+
+    }
+
+    
+}
