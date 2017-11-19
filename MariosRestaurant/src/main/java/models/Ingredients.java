7ff5f7639d579944e1c35a193ff2db97b760844b
Joao <johnynecas@hotmail.com>
2017-11-19 14:52:35 +0000

    created project basic package structure

---
+++ MariosRestaurant/src/main/java/models/Ingredients.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Ingredients {
+    private String name;
+//    private double price; to be added if wanted to create a way of manger to calculate the profits for restuarant;
+
+
+    public Ingredients(String name){
+        this.name= name;
+    }
+
+
+}
---
+++ MariosRestaurant/src/main/java/models/Patron.java
@@ -0,0 +1,16 @@
+package models;
+
+import interfaces.MenuItem;
+
+import java.util.ArrayList;
+
+public class Patron {
+    private double bill;
+    private ArrayList<MenuItem> order;
+
+    public Patron(){
+        this.bill = 0;
+        this.order = new ArrayList(); 
+
+    }
+}
