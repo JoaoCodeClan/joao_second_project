617c557a41d2c468ab29fc00ccdecc70885a554c
Joao <johnynecas@hotmail.com>
2017-11-19 19:18:28 +0000

    created test class for menu class

---
+++ MariosRestaurant/src/test/java/MenuTest.java
@@ -0,0 +1,18 @@
+import models.Menu;
+import org.junit.Before;
+
+public class MenuTest {
+
+    Menu menu;
+
+
+    @Before
+
+    public void before(){
+
+        menu= new Menu();
+    }
+
+
+    
+}
