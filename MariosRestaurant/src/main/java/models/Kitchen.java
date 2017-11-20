fdaf8e810a3334a6738f1fd0b2e802dc6f3f7206
Joao <johnynecas@hotmail.com>
2017-11-20 11:29:02 +0000

    created kitchen class and kitchen test class

---
+++ MariosRestaurant/src/main/java/models/Kitchen.java
@@ -0,0 +1,11 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Kitchen {
+    private ArrayList<Ingredient>stock;
+
+    public Kitchen(){
+        this.stock= new ArrayList();
+    }
+}
